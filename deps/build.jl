38663c131fc350af32408ef2a5f5603608d780da
Erik Schnetter <schnetter@gmail.com>
2014-11-15 21:18:56 -0500

    Be compatible with Julia 0.3

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
 using BinDeps
-
 @BinDeps.setup
+
+include("../src/compatibility.jl")
 
 
 
@@ -8,7 +9,7 @@ libhwloc = library_dependency("libhwloc")
 libhwloc_helpers = library_dependency("libhwloc_helpers")
 
 # Install via a package manager
-provides(AptGet, Dict("libhwloc-dev" => libhwloc))
+provides(AptGet, @Dict("libhwloc-dev" => libhwloc))
 
 #fails @osx_only begin
 #fails     using Homebrew
@@ -22,10 +23,10 @@ provides(AptGet, Dict("libhwloc-dev" => libhwloc))
 # Build from source
 destdir = joinpath(BinDeps.depsdir(libhwloc_helpers), "usr")
 provides(Sources,
-         Dict(URI("http://www.open-mpi.org/software/hwloc/v1.10/downloads/hwloc-1.10.0.tar.gz") => libhwloc))
-provides(BuildProcess, Dict(Autotools(libtarget="src/libhwloc.la",
-                                      configure_options=["--without-x"]) =>
-                            libhwloc))
+         @Dict(URI("http://www.open-mpi.org/software/hwloc/v1.10/downloads/hwloc-1.10.0.tar.gz") => libhwloc))
+provides(BuildProcess, @Dict(Autotools(libtarget="src/libhwloc.la",
+                                       configure_options=["--without-x"]) =>
+                             libhwloc))
 
 
 
@@ -39,5 +40,5 @@ provides(SimpleBuild,
 
 
 
-@BinDeps.install Dict(:libhwloc => :libhwloc,
-                      :libhwloc_helpers => :libhwloc_helpers)
+@BinDeps.install Dict([(:libhwloc, :libhwloc),
+                       (:libhwloc_helpers, :libhwloc_helpers)])
