763b75a41cd36dd89d116e868ed6675732bc8be1
Carsten Bauer <crstnbr@gmail.com>
2021-04-02 19:48:19 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Hwloc
 using Test
+using CpuId
 
 version = Hwloc.get_api_version()
 @test isa(version, VersionNumber)
@@ -17,6 +18,31 @@ println(counts)
 @test counts[:Core] > 0
 @test counts[:PU] > 0
 @test num_physical_cores() == counts[:Core]
+@test num_packages() == counts[:Package]
+
+# Cache sizes
+allequal(xs) = all(x == first(xs) for x in xs)
+l1, l2, l3 = CpuId.cachesize()
+
+l1s = l1cache_sizes()
+@test length(l1s) == counts[:L1Cache]
+if allequal(l1s) # running on a machine with equal caches
+    @test first(l1s) == l1
+end
+l2s = l2cache_sizes()
+@test length(l2s) == counts[:L2Cache]
+if allequal(l2s) # running on a machine with equal caches
+    @test first(l2s) == l2
+end
+l3s = l3cache_sizes()
+@test length(l3s) == counts[:L3Cache]
+if allequal(l3s) # running on a machine with equal caches
+    @test first(l3s) == l3
+end
+
+@test typeof(collectobjects(topology, :L1Cache)) == Vector{Hwloc.Object}
+@test length(collectobjects(topology, :L1Cache)) == counts[:L1Cache]
+@test first(collectobjects(topology, :L1Cache)).type_ == :L1Cache
 
 # Hierarchical summary of topology
 hinfo = Hwloc.getinfo(topology)
