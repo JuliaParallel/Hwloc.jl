d050e0392c8df4a503917bc135f5afcc0ca89fd3
Carsten Bauer <crstnbr@gmail.com>
2022-08-18 10:49:41 +0200

    drop formatting changes

--- src/highlevel_api.jl
+++ src/highlevel_api.jl
@@ -16,14 +16,14 @@ end
 
 Prints the topology of the given `obj` as a tree to `io`.
 """
-function print_topology(io::IO=stdout, obj::Object=gettopology(); indent="", newline=false, prefix="")
+function print_topology(io::IO = stdout, obj::Object = gettopology(); indent = "", newline = false, prefix = "")
     t = hwloc_typeof(obj)
     idxstr = t in (:Package, :Core, :PU) ? "L#$(obj.logical_index) P#$(obj.os_index) " : ""
     attrstr = string(obj.attr)
 
     if t in (:L1Cache, :L2Cache, :L3Cache, :I1Cache)
         tstr = first(string(t), 2)
-        attrstr = "(" * _bytes2string(obj.attr.size) * ")"
+        attrstr = "("*_bytes2string(obj.attr.size)*")"
     else
         tstr = string(t)
     end
@@ -31,20 +31,20 @@ function print_topology(io::IO=stdout, obj::Object=gettopology(); indent="", new
     newline && print(io, "\n", indent)
     print(io, prefix, tstr, " ",
         idxstr,
-        attrstr, obj.mem > 0 ? "(" * _bytes2string(obj.mem) * ")" : "")
+        attrstr, obj.mem > 0 ? "("*_bytes2string(obj.mem)*")" : "")
 
     for memchild in obj.memory_children
-        memstr = "(" * _bytes2string(memchild.mem) * ")"
+        memstr = "("*_bytes2string(memchild.mem)*")"
         println(io)
-        print(io, indent * repeat(" ", 4), string(memchild.type_), " ", memstr)
+        print(io, indent*repeat(" ", 4), string(memchild.type_), " ", memstr)
     end
 
     for child in obj.children
-        no_newline = length(obj.children) == 1 && t in (:L3Cache, :L2Cache, :L1Cache)
+        no_newline = length(obj.children)==1 && t in (:L3Cache, :L2Cache, :L1Cache)
         if no_newline
-            print_topology(io, child; indent=indent, newline=false, prefix=" + ")
+            print_topology(io, child; indent = indent, newline=false, prefix = " + ", )
         else
-            print_topology(io, child; indent=indent * repeat(" ", 4), newline=true)
+            print_topology(io, child; indent = indent*repeat(" ", 4), newline=true)
         end
     end
     return nothing
@@ -74,21 +74,21 @@ topology() = print_topology(gettopology())
 Prints a summary of the system topology (loosely similar to `hwloc-info`).
 """
 function topology_info()
-    nodes = Tuple{Symbol,Int64,String}[]
+    nodes = Tuple{Symbol, Int64, String}[]
     for subobj in gettopology()
-        idx = findfirst(t -> t[1] == subobj.type_, nodes)
+        idx = findfirst(t->t[1] == subobj.type_, nodes)
         if isnothing(idx)
             attrstr = ""
             subobj.mem > 0 && (attrstr = " ($(_bytes2string(subobj.mem)))")
             subobj.type_ ∈ (:L1Cache, :L2Cache, :L3Cache) && (attrstr = " ($(_bytes2string(subobj.attr.size)))")
             push!(nodes, (subobj.type_, 1, attrstr))
         else
-            nodes[idx] = (subobj.type_, nodes[idx][2] + 1, nodes[idx][3])
+            nodes[idx] = (subobj.type_, nodes[idx][2]+1, nodes[idx][3])
         end
     end
 
-    for (i, n) in enumerate(nodes)
-        println(repeat(" ", i - 1), "$(n[1]): ", n[2], n[3])
+    for (i,n) in enumerate(nodes)
+        println(repeat(" ", i-1), "$(n[1]): ", n[2], n[3])
     end
     return nothing
 end
@@ -100,8 +100,8 @@ whose entries indicate which and how often certain hwloc elements are present.
 If the keyword argument `list_all` (default: `false`) is set to `true`,
 the resulting dictionary will contain all possible hwloc elements.
 """
-function getinfo(; list_all::Bool=false)
-    res = list_all ? Dict{Symbol,Int}(t => 0 for t in obj_types) : Dict{Symbol,Int}()
+function getinfo(; list_all::Bool = false)
+    res = list_all ? Dict{Symbol,Int}(t => 0 for t in obj_types) : Dict{Symbol, Int}()
     for subobj in gettopology()
         t = hwloc_typeof(subobj)
         if t in keys(res)
@@ -139,7 +139,7 @@ hwloc_isa(type::Symbol) = obj -> hwloc_typeof(obj) == type
 
 Collects objects of the given hwloc `type` from the (sub-)topology tree `t`.
 """
-collectobjects(type::Symbol, t::Object=gettopology()) = collect(Iterators.filter(hwloc_isa(type), t))
+collectobjects(type::Symbol, t::Object = gettopology()) = collect(Iterators.filter(hwloc_isa(type), t))
 
 """
 The number of physical cores.
@@ -164,29 +164,29 @@ num_numa_nodes() = count(hwloc_isa(:NUMANode), gettopology())
 """
 Returns a vector containing the sizes of all available L3 caches in Bytes.
 """
-l3cache_sizes() = map(obj -> obj.attr.size, Iterators.filter(obj -> obj.type_ == :L3Cache, gettopology()))
+l3cache_sizes() = map(obj->obj.attr.size, Iterators.filter(obj->obj.type_ == :L3Cache, gettopology()))
 """
 Returns a vector containing the L3 cache line sizes in Bytes.
 """
-l3cache_linesizes() = map(obj -> obj.attr.linesize, Iterators.filter(obj -> obj.type_ == :L3Cache, gettopology()))
+l3cache_linesizes() = map(obj->obj.attr.linesize, Iterators.filter(obj->obj.type_ == :L3Cache, gettopology()))
 
 """
 Returns a vector containing the sizes of all available L2 caches in Bytes.
 """
-l2cache_sizes() = map(obj -> obj.attr.size, Iterators.filter(obj -> obj.type_ == :L2Cache, gettopology()))
+l2cache_sizes() = map(obj->obj.attr.size, Iterators.filter(obj->obj.type_ == :L2Cache, gettopology()))
 """
 Returns a vector containing the L2 cache line sizes in Bytes.
 """
-l2cache_linesizes() = map(obj -> obj.attr.linesize, Iterators.filter(obj -> obj.type_ == :L2Cache, gettopology()))
+l2cache_linesizes() = map(obj->obj.attr.linesize, Iterators.filter(obj->obj.type_ == :L2Cache, gettopology()))
 
 """
 Returns a vector containing the sizes of all available L1 caches in Bytes.
 """
-l1cache_sizes() = map(obj -> obj.attr.size, Iterators.filter(obj -> obj.type_ == :L1Cache, gettopology()))
+l1cache_sizes() = map(obj->obj.attr.size, Iterators.filter(obj->obj.type_ == :L1Cache, gettopology()))
 """
 Returns a vector containing the L1 cache line sizes in Bytes.
 """
-l1cache_linesizes() = map(obj -> obj.attr.linesize, Iterators.filter(obj -> obj.type_ == :L1Cache, gettopology()))
+l1cache_linesizes() = map(obj->obj.attr.linesize, Iterators.filter(obj->obj.type_ == :L1Cache, gettopology()))
 
 """
     cachesize()
